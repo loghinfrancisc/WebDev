c942d1e91a8ea17967c631347596a4d01a7a0da8
robis1221 <robis1221@inbox.lv>
2019-04-20 12:26:15 +0200

    robert backend

--- models/user.js
+++ models/user.js
@@ -2,49 +2,38 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const config = require('../config/database');
 
-// User Schema
 const UserSchema = mongoose.Schema({
-  name: {
-    type: String
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-});
+    username:{
+        type: String
+    },
+    password:{
+        type: String
+    }
 
+});
 const User = module.exports = mongoose.model('User', UserSchema);
 
-module.exports.getUserById = function(id, callback){
-  User.findById(id, callback);
+module.exports.getUserById=function(id,callback){
+    User.findById(id,callback);
 }
-
 module.exports.getUserByUsername = function(username, callback){
-  const query = {username: username}
-  User.findOne(query, callback);
+    const query = {username:username}
+    User.findOne(query, callback);
 }
-
-module.exports.addUser = function(newUser, callback){
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(newUser.password, salt, (err, hash) => {
-      if(err) throw err;
-      newUser.password = hash;
-      newUser.save(callback);
+//remove this comment to be able to register, see routes/users.js
+/*module.exports.addUser=function(newUser, callback){
+bcrypt.genSalt(10, (err,salt)=>{
+    bcrypt.hash(newUser.password, salt, (err, hash)=>{
+        if(err) throw err;
+        newUser.password=hash;
+        newUser.save(callback);
+    })
+});
+}
+*/
+module.exports.comparePassword=function(candidatePassword,hash,callback){
+    bcrypt.compare(candidatePassword, hash, (err,isMatch)=>{
+if(err) throw err;
+callback(null,isMatch);
     });
-  });
-}
-
-module.exports.comparePassword = function(candidatePassword, hash, callback){
-  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
-    if(err) throw err;
-    callback(null, isMatch);
-  });
 }
